bcf8ec4005e7323367864bb44b9dd513d60464c5
gautam0209 <vashisht.g@husky.neu.edu>
2018-12-08 00:56:21 -0500

    searchRests

---
+++ cs5200-fall2018-hungry-cubs/src/main/java/edu/northeastern/cs5200/hungrycubs/models/InputRestaurant.java
@@ -0,0 +1,44 @@
+package edu.northeastern.cs5200.hungrycubs.models;
+
+
+// POJO To search Restaurant
+
+public class InputRestaurant {
+	
+	private String streetAddress;
+	private String latitude;
+	private String longitude;
+	private String pickUpRadius;
+	private String search;
+	public String getStreetAddress() {
+		return streetAddress;
+	}
+	public void setStreetAddress(String streetAddress) {
+		this.streetAddress = streetAddress;
+	}
+	public String getLatitude() {
+		return latitude;
+	}
+	public void setLatitude(String latitude) {
+		this.latitude = latitude;
+	}
+	public String getLongitude() {
+		return longitude;
+	}
+	public void setLongitude(String longitude) {
+		this.longitude = longitude;
+	}
+	public String getPickUpRadius() {
+		return pickUpRadius;
+	}
+	public void setPickUpRadius(String pickUpRadius) {
+		this.pickUpRadius = pickUpRadius;
+	}
+	public String getSearch() {
+		return search;
+	}
+	public void setSearch(String search) {
+		this.search = search;
+	}
+
+}
