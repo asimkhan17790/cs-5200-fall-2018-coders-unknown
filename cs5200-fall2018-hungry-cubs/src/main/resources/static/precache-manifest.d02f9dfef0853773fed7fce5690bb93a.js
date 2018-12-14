eec4b108f6f62f22213d06e911e52ff2bce63c64
Asim Khan <asimkhan17790@users.noreply.github.com>
2018-12-14 02:35:25 -0500

    new files

---
+++ cs5200-fall2018-hungry-cubs/src/main/resources/static/precache-manifest.d02f9dfef0853773fed7fce5690bb93a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "e69d592a3cecfda9948d",
+    "url": "/static/js/main.e69d592a.chunk.js"
+  },
+  {
+    "revision": "c165723689a7e42e8d8d",
+    "url": "/static/js/1.c1657236.chunk.js"
+  },
+  {
+    "revision": "e69d592a3cecfda9948d",
+    "url": "/static/css/main.76aacaf5.chunk.css"
+  },
+  {
+    "revision": "c165723689a7e42e8d8d",
+    "url": "/static/css/1.1175639b.chunk.css"
+  },
+  {
+    "revision": "577aa58a1b2eda0c3847d1c4f234bce1",
+    "url": "/index.html"
+  }
+];
